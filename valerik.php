5749b637ded0928c29633188bf614c75d538e5ff
Gevorg Martirosyan <gevorg@bitband.com>
2022-02-04 01:07:01 +0400

    long messssssssssssssssssssagggeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee

--- valerik.php
+++ valerik.php
@@ -52,5 +52,5 @@ asd
 
 hasdasdasd
 a
-
+vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv Comitttttttttttttttttttttttttttttttttttttttttttttttttttt
 
